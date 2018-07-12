corrected signs in force

toy_model.py
@@ -6,28 +6,28 @@ import sys
 
 def force(x):
     """ F = -dU(x)/dx """
-    F = -E*np.sum([2*(k+1) * c * x**(2*k+1) * (-1)**(n-k)
-                   for k, c in enumerate(constants)])
-    return 0.0#F
+    F = -E * np.sum([2*(k+1) * c * x**(2*k+1) * (-1)**(n-k-1)
+                     for k, c in enumerate(constants)])
+    return F
 
-constants = [.0, 0.1]
+constants = [.0, 0.01]
 n = len(constants)
-E = 1.0E-5
-D = 1.0E-5
-kBT = 1.0E-1
+E = 1.0E+1
+D = 1.0E-4
+kBT = 1E+1
 s_2D = np.sqrt(2*D)
 D_kBT = D/kBT
 
-dt = 1E-1
-s_dt = np.sqrt(dt)
-x = [1/np.sqrt(2*constants[1])]
-num_steps = 100000
+dt = 1E0
+s_2Ddt = np.sqrt(2*D*dt)
+x = [-1/np.sqrt(2*constants[1])]
+num_steps = 1000000
 
 print('{} steps, E={}, kBT={}, sqrt(2D)={}, D/kBT={}, dt={}'.format(n, E, kBT, s_2D, D_kBT, dt))
 for t in range(num_steps):
 	sys.stderr.write('step {}/{}            \r'.format(t, num_steps))
 
-	v_dt = force(x[-1])*D_kBT*dt + np.random.normal(0, s_2D)*s_dt
+	v_dt = force(x[-1])*D_kBT*dt + np.random.normal(0, 1)*s_2D
 	x.append(x[-1] + v_dt)
 	print(t, x[-1], v_dt)
 	if np.isnan(x[-1]):