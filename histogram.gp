f094e9bcb1e9296682a2a4ed947f8b6a68637bf5
pelegs <pelegs@gmail.com>
2018-07-14 12:11:53 +0200

    Finally it works (a constant was set to 0 instead of 1)

--- histogram.gp
+++ histogram.gp
@@ -1,6 +1,6 @@
 set terminal png size 1000,1000
 
-E=1
+E=6
 c=1000
 c1=0.01
 c2=0.2
@@ -9,7 +9,7 @@ y_min = 1/sqrt(2*c)
 U1(x)=(E*(c*x**4 - x**2) + dE)/dE
 U2(x)=E*(c1*x**6 - c2*x**4 + x**2)
 
-binwidth=0.1
+binwidth=1
 set boxwidth binwidth
 bin(x, width) = width*floor(x/width) + binwidth/2.0
 
@@ -17,12 +17,22 @@ set output 'data_position.png'
 stats "data" u 2
 set xrange[STATS_min:STATS_max]
 binwidth = (STATS_max - STATS_min)/200
-set boxwidth binwidth
-plot "data" u (bin($2, binwidth)):(1.0/STATS_max) smooth freq w boxes notitle
 
-set output 'data_velocity.png'
-stats "data" u 3
-set xrange[STATS_min:STATS_max]
-binwidth = (STATS_max - STATS_min)/200
-set boxwidth binwidth
-plot "data" u (bin($3, binwidth)):(1.0/STATS_max) smooth freq w boxes notitle
+set table "temp"
+plot "data" u (bin($2, binwidth)):(1.0) smooth freq w boxes notitle
+unset table
+stats "temp" u 2
+y_max = STATS_max
+
+set xlabel "Position"
+set ylabel "Normalized frequency"
+set boxwidth binwidth*0.9
+set style fill solid 0.5
+plot "data" u (bin($2, binwidth)):(1.0/y_max) smooth freq w boxes notitle
+
+#set output 'data_velocity.png'
+#stats "data" u 3
+#set xrange[STATS_min:STATS_max]
+#binwidth = (STATS_max - STATS_min)/200
+#set boxwidth binwidth
+#plot "data" u (bin($3, binwidth)):(1.0/STATS_max) smooth freq w boxes notitle
--- toy_model.py
+++ toy_model.py
@@ -10,25 +10,43 @@ def force(x):
                      for k, c in enumerate(constants)])
     return F
 
-constants = [.0, 0.01]
+#def force(x):
+#    return -E * (4*constants[1]*x**3 - 2*x)
+
+constants = [1.0, 0.2, 0.01]
 n = len(constants)-1
-E = 1.0E+2
+E = 1.0E+1
 D = 1.0E-1
-kBT = 1E+0
+kBT = 1E-0
 s_2D = np.sqrt(2*D)
 D_kBT = D/kBT
 
 dt = 1E-1
 s_2Ddt = np.sqrt(2*D*dt)
 x = [0]#[-1/np.sqrt(2*constants[1])]
-num_steps = 1000000
+v_dt = [0]
+num_steps = 3000000
 
-print('#{} steps, E={}, kBT={}, sqrt(2D)={}, D/kBT={}, dt={}'.format(n, E, kBT, s_2D, D_kBT, dt))
+#print('#{} steps, E={}, kBT={}, sqrt(2D)={}, D/kBT={}, dt={}'.format(n, E, kBT, s_2D, D_kBT, dt))
 for t in range(num_steps):
 	sys.stderr.write('step {}/{}            \r'.format(t, num_steps))
 
-	v_dt = force(x[-1])*D_kBT*dt**2 + np.random.normal(0, 1)*s_2D
-	x.append(x[-1] + v_dt)
-	print(t, x[-1], v_dt)
-	if np.isnan(x[-1]):
+	v_dt.append(force(x[-1])*D_kBT*dt**2 + np.random.normal(0, 1)*s_2D)
+	x.append(x[-1] + v_dt[-1])
+	print(t, x[-1], v_dt[-1])
+	if (np.isnan(x[-1]) or np.isnan(v_dt[-1])):
 		break
+
+#x_bins = 500
+#x_interval = np.linspace(min(x), max(x), x_bins)
+#min_x = min(x)
+#L = max(x) - min(x)
+#v_vs_x = [[] for _ in x_interval]
+#for pos, vel in zip(x, v_dt):
+#    key = int(np.floor((pos-min_x)/L*x_bins))
+#    if key < x_bins:
+#        v_vs_x[key].append(np.abs(vel))
+#
+#for i, vels in enumerate(v_vs_x):
+#    if len(vels) != 0:
+#        print(x_interval[i], np.average(vels), np.std(vels))
