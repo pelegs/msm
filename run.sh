b0338686d7837c8cab9aa2fd38de918ae90f4d58
Peleg Sapir <psapir@psapir.mpibpc.intern>
2018-08-30 18:20:58 +0200

    Added command line arguments to run script

--- run.sh
+++ run.sh
@@ -1,29 +1,88 @@
 #!/usr/bin/env bash
 
-name=$1
-num_particles=$2
-max_t=$3
-action=$4
+# =========== Parse arguments =========== #
 
-if [[ $action == "run" ]]
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    --name)
+    NAME="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -n|--nump)
+    NUMP="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --maxt)
+    MAXT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --dt)
+    DT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -E|--energy)
+    ENERGY="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -D|--diffusion)
+    DIFFUSION="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --kbt)
+    KBT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --action)
+    ACTION="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+# =========== Actual script =========== #
+
+if [[ $ACTION == "run" ]]
 then
-    if [ ! -f data/$name.gp ]; then
-          echo "Gnuplot script $name.gp not found, creating file"
-          cp gp/base.gp gp/$name.gp
-          sed "s/NAMEX/$name/g" gp/base.gp > gp/$name.gp
+    if [ ! -f data/$NAME.gp ]; then
+          echo "Gnuplot script $NAME.gp not found, creating file"
+          cp gp/base.gp gp/$NAME.gp
+          sed "s/NAMEX/$NAME/g" gp/base.gp > gp/$NAME.gp
     fi
-    echo "Running simulation $name, with $num_particles particles and max time $max_t"
-	  python3 simulation/simulate.py --name $name -n $num_particles --maxt $max_t && gnuplot gp/$name.gp && eog graphs/$name.png
+	  echo "Name: $NAME"
+	  echo "Number of particles: $nump"
+	  echo "Max time: $MAXP"
+	  echo "Time step: $DT"
+	  echo "Energy: $ENERGY"
+	  echo "Diffusion coefficient: $DIFFUSION"
+	  echo "Generalized temperature: $KBT"
+    python3 simulation/simulate.py --name $NAME -n $NUMP --maxt $MAXT --dt $DT -E $ENERGY -D $DIFFUSION --KBT $KBT && gnuplot gp/$NAME.gp && eog graphs/$NAME.png
 
 elif [[ $action == "graph" ]]
 then
-    if [ ! -f data/$name.gp ]; then
-          echo "Gnuplot script $name.gp not found, creating file"
-          cp gp/base.gp gp/$name.gp
-          sed "s/NAMEX/$name/g" gp/base.gp > gp/$name.gp
+    if [ ! -f data/$NAME.gp ]; then
+          echo "Gnuplot script $NAME.gp not found, creating file"
+          cp gp/base.gp gp/$NAME.gp
+          sed "s/NAMEX/$NAME/g" gp/base.gp > gp/$NAME.gp
     fi
     echo "Running gnuplot..."
-    gnuplot gp/$name.gp && eog graphs/$name.png
+    gnuplot gp/$NAME.gp && eog graphs/$NAME.png
 else
     echo "$action is not a valid action!"
 fi
