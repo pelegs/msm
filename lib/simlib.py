e375a6dbf9d653252be6874ce7935c9118cbaab5
Peleg Sapir <psapir@psapir.mpibpc.intern>
2018-11-21 15:23:52 +0100

    new simulations

--- lib/simlib.py
+++ lib/simlib.py
@@ -101,6 +101,58 @@ def simulate(potential, method = 'langevin',
 
     return ts, xs
 
+def print_simulate(name,
+                   potential, method = 'langevin',
+                   max_t = 1000, dt = 0.1,
+                   num_particles = 1, num_bins = 20,
+                   dstep = 1.0, D = 1.0, beta=1.0, x0 = 0.0):
+    """
+    Actuall simulation.
+    More details to come.
+    """
+
+    C1 = D * beta * dt
+    C2 = np.sqrt(2*D*dt)
+
+    sqrt_N = np.sqrt(num_particles)
+
+    xs = np.ones(num_particles) * x0
+    xs_prev = np.ones(num_particles) * x0
+    ts = np.arange(0, max_t, dt)
+
+    real_mean = x0 * np.exp(-potential.k*D*beta*ts)
+    real_var  = 1/(potential.k*beta) * (1-np.exp(-2*potential.k*D*beta*ts))
+
+    filename = '../data/{}_{}_D_{}_k_{}_beta_{}_x0_{}.data'.format(name, method, D, potential.k, beta, x0)
+    with open(filename, 'wb', 0) as f:
+        line = '# Num particles: {}, x0={}, D={}, beta={}, k={}\n'.format(num_particles,
+                                                                          x0, D, beta,
+                                                                          potential.k)
+        f.write(line.encode())
+
+        for i, t in enumerate(tqdm(ts[:-1])):
+            for j in range(num_particles):
+                if method in ['lang', 'langevin']:
+                    xs[j] = xs_prev[j] + C1 * potential.get_force(xs_prev[j]) + C2 * np.random.normal()
+                    xs_prev[j] = xs[j]
+                elif method in ['smol', 'smlouchowski']:
+                    c = potential.get_derivative(xs_prev[j])
+                    mu = xs_prev[j] - C1*c
+                    xs[j] = np.random.normal(mu, C2)
+                    xs_prev[j] = xs[j]
+                else:
+                    raise ValueError('Unknown method \'{}\''.format(method))
+
+            m = np.mean(xs)
+            dm = np.std(xs) / sqrt_N
+            v = np.var(xs)
+            dv = 2/(num_particles-1) * np.var(xs)**2
+
+            line = '{} {} {} {} {} {} {}\n'.format(t,
+                                                   m, dm, real_mean[i],
+                                                   v, dv, real_var[i])
+            f.write(line.encode())
+
 def MSD(xs):
     return [np.mean([(xs[t+1,i] - xs[0,i])**2 for i,_ in enumerate(xs[t,:])])
                                               for t,_ in enumerate(xs[:-1,0])]
--- simulation/harmonic_validation.py
+++ simulation/harmonic_validation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import numpy as np
 import sys
 sys.path.append('../lib')
@@ -6,23 +7,14 @@ from simlib import *
 
 
 method = sys.argv[1]
-num_particles = 1000000
+k = float(sys.argv[2])
+D = float(sys.argv[3])
+beta = float(sys.argv[4])
+x0 = float(sys.argv[5])
+
+num_particles = 100000
 max_t = 10
-dt = 0.1
-x0 = 2
-D = 1.0
-beta = 1
-k = 0.5
+dt = 0.001
 num_steps = int(max_t/dt)
 
-ts, xs = simulate(harmonic_potential(k=k), method=method, num_particles=num_particles, dt=dt, max_t=max_t, x0=x0, D=D, beta=beta)
-
-m = np.mean(xs, axis=1)
-v = np.var(xs, axis=1)
-dm = np.std(xs, axis=1) / np.sqrt(num_particles)
-dv = 2/(num_particles-1)*v**2
-
-with open('../data/harmonic_validation_{}.data'.format(method), 'w') as f:
-    f.write('# num particles: {}, D={}, beta={}, k={}\n'.format(num_particles, D, beta, k))
-    for i, t in enumerate(ts):
-        f.write('{} {} {} {} {}\n'.format(t, m[i], dm[i], v[i], dv[i]))
+print_simulate('harmonic_validation', harmonic_potential(k=k), method=method, num_particles=num_particles, dt=dt, max_t=max_t, x0=x0, D=D, beta=beta)
