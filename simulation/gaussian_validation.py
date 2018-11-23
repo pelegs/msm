089303b5346123e629d5f38a1fe623d1712242b8
pelegs <pelegs@gmail.com>
2018-11-23 21:27:13 +0100

    New sims

--- simulation/gaussian_validation.py
+++ simulation/gaussian_validation.py
@@ -89,3 +89,27 @@ filename = '../data/gaussian_triple_well_{}_num_particles_{}_dt_{}_D_{}_b_{}_x0_
 with open(filename, 'w') as f:
     for b, h in zip(bins, mean_hist):
         f.write('{} {}\n'.format(b, h))
+
+# ============= Multiple well ============= #
+
+m = [7, -14,  -7,  -1, -11,   2,  14]
+s = [2, 2, 2, 1, 1, 3, 2]
+gs = [gaussian(mi, si) for mi, si in zip(m, s)]
+U = gaussian_potential(gs)
+
+ts, xs = simulate(method=method, potential=U,
+                  num_particles=num_particles, x0=x0, D=D, beta=beta,
+                  dt=dt, max_t=max_t)
+
+hist = np.zeros(shape=(num_particles, num_bins-1))
+for i in range(num_particles):
+    hist[i], _ = np.histogram(xs[:,i], bins)
+mean_hist = np.mean(hist, axis=0)
+
+filename = '../data/gaussian_multiple_well_{}_num_particles_{}_dt_{}_D_{}_b_{}_x0_{}.data'.format(method,
+                                                                                                num_particles,
+                                                                                                dt,
+                                                                                                D, beta, x0)
+with open(filename, 'w') as f:
+    for b, h in zip(bins, mean_hist):
+        f.write('{} {}\n'.format(b, h))
