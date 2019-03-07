aad626d1370930017aba6fc3478ff8c1b153fb38
pelegs <pelegs@gmail.com>
2019-03-07 17:06:00 +0100

    potential seems to work?

--- tests/multi_dim_potentials/multi_dim_potentials.py
+++ tests/multi_dim_potentials/multi_dim_potentials.py
@@ -10,16 +10,12 @@ from scipy.stats import linregress
 def gauss(x, a, m, s):
     return a * sqrt_2pi/s * exp(-(x-m)**2/(2*s**2))
 
-def gauss_axis(x, A, M, S):
-    return np.sum([gauss(x, a, m, s) for a, m, s in zip(A, M, S)])
+def gauss_force_dim(x, A, M, S, beta=1.0):
+    return np.sum([(m-x)/s**2 for a, m, s in zip(A, M, S)])
 
-def potential(X, A, M, S, beta=1):
-    return -beta*log(np.prod([gauss_axis(x, A[dim], M[dim], S[dim])
-                              for dim, x in enumerate(X)]))
-
-def force(X, A, M, S, beta=1):
-    pass
-
+def force(X, A, M, S, beta=1.0):
+    return np.array([gauss_force_dim(x, a, m, s)
+                     for x, a, m, s in zip(X, A, M, S)])
 
 """
 Example for parameters:
@@ -34,30 +30,49 @@ Sig = np.array([[1, 1],
                 [1, 1]])
 """
 
-num_particles = 10000
-num_dim = 1
-num_steps = 100
-num_gaussians = 2
+num_particles = 1
+num_dim = 1000
+num_steps = 1000
+num_gaussians = 1
 
-dt = 0.01
-beta = 1
+dt = 0.001
+beta = 1.0/2.7
 D = np.random.uniform(0.1, 13.37)
 A = D*beta*dt
 B = np.sqrt(2*D*dt)
 
+Amp = np.ones(shape=(num_dim, num_gaussians))
+Mu = np.zeros(shape=(num_dim, num_gaussians))
+Sig = np.ones(shape=(num_dim, num_gaussians))
+
 Xs = np.zeros(shape=(num_steps, num_particles, num_dim))
+Xs[0,0,:] = np.ones(num_dim) * 3.7
 
 for t in tqdm(range(1, num_steps)):
     for p in range(num_particles):
-        drift = 0 #A*potential(Xs[t-1,p,:], Amp, Mu, Sig)
+        drift = A*force(Xs[t-1,p,:], Amp, Mu, Sig, beta)
         noise = B*np.random.normal(size=num_dim)
         Xs[t,p,:] = Xs[t-1,p,:] + drift + noise
 
+        #forces = force(Xs[t-1,p,:], Amp, Mu, Sig, beta)
+        #for x, F in zip(Xs[t-1,p,:], forces):
+        #    print(x, F, end=' ')
+        #print('')
+
+"""
+# Linear regression
 ts = range(num_steps)
-pos = Xs[:,:,0]
+pos = Xs[:,0,:]
 mean = np.mean(pos, axis=1)
 var = np.var(pos, axis=1)
-
-# Linear regression
 slope, intercept, r_value, p_value, std_err = linregress(ts, var)
 print('var: y={:0.3f} (2Ddt={:0.3f}) + {:0.3f}, r^2={:0.3f}'.format(slope, 2*D*dt, intercept, r_value))
+"""
+
+mean = np.mean(Xs[:,0,:], axis=1)
+var = np.var(Xs[:,0,:], axis=1)
+for t, m, v in zip(range(num_steps), mean, var):
+    print(t, m, v)
+
+#for t, x in enumerate(Xs[:,0,:]):
+#    print(t, ' '.join(map(str, x)))
