f08305ff495cc2699643c65ce8adeba7c6ba3d88
pelegs <pelegs@gmail.com>
2019-03-08 13:46:05 +0100

    Added check functions

--- tests/multi_dim_potentials/multi_dim_potentials.py
+++ tests/multi_dim_potentials/multi_dim_potentials.py
@@ -5,13 +5,19 @@ from numpy import exp, sqrt, pi, log
 sqrt_2pi = 1/sqrt(2*pi)
 from tqdm import tqdm
 from scipy.stats import linregress
+from sys import stderr
 
 
 def gauss(x, a, m, s):
     return a * sqrt_2pi/s * exp(-(x-m)**2/(2*s**2))
 
 def gauss_force_dim(x, A, M, S, beta=1.0):
-    return np.sum([(m-x)/s**2 for a, m, s in zip(A, M, S)])
+    dg = np.sum([(m-x)/s**2*gauss(x, a, m, s) for a, m, s in zip(A, M, S)])
+    g  = np.sum([gauss(x, a, m, s) for a, m, s in zip(A, M, S)])
+    if g == 0:
+        return 0
+    else:
+        return beta * dg/g
 
 def force(X, A, M, S, beta=1.0):
     return np.array([gauss_force_dim(x, a, m, s)
@@ -30,19 +36,21 @@ Sig = np.array([[1, 1],
                 [1, 1]])
 """
 
-num_particles = 1
-num_dim = 1000
-num_steps = 1000
-num_gaussians = 1
+num_particles = 100
+num_dim = 1
+num_steps = 5000
+num_gaussians = 2
 
-dt = 0.001
-beta = 1.0/2.7
-D = np.random.uniform(0.1, 13.37)
+dt = 0.01
+beta = 1.0
+D = 5.0
 A = D*beta*dt
 B = np.sqrt(2*D*dt)
 
 Amp = np.ones(shape=(num_dim, num_gaussians))
 Mu = np.zeros(shape=(num_dim, num_gaussians))
+Mu[:,0] = -3
+Mu[:,1] = 3
 Sig = np.ones(shape=(num_dim, num_gaussians))
 
 Xs = np.zeros(shape=(num_steps, num_particles, num_dim))
@@ -54,25 +62,33 @@ for t in tqdm(range(1, num_steps)):
         noise = B*np.random.normal(size=num_dim)
         Xs[t,p,:] = Xs[t-1,p,:] + drift + noise
 
+        ## Check Force vs. Position
         #forces = force(Xs[t-1,p,:], Amp, Mu, Sig, beta)
         #for x, F in zip(Xs[t-1,p,:], forces):
         #    print(x, F, end=' ')
         #print('')
 
+
 """
-# Linear regression
-ts = range(num_steps)
-pos = Xs[:,0,:]
-mean = np.mean(pos, axis=1)
-var = np.var(pos, axis=1)
-slope, intercept, r_value, p_value, std_err = linregress(ts, var)
-print('var: y={:0.3f} (2Ddt={:0.3f}) + {:0.3f}, r^2={:0.3f}'.format(slope, 2*D*dt, intercept, r_value))
+Sanity checks
 """
 
-mean = np.mean(Xs[:,0,:], axis=1)
-var = np.var(Xs[:,0,:], axis=1)
-for t, m, v in zip(range(num_steps), mean, var):
-    print(t, m, v)
+def linear_regression():
+    # Linear regression (e.g. for MSD check)
+    ts = range(num_steps)
+    pos = Xs[:,0,:]
+    mean = np.mean(pos, axis=1)
+    var = np.var(pos, axis=1)
+    slope, intercept, r_value, p_value, std_err = linregress(ts, var)
+    print('var: y={:0.3f} (2Ddt={:0.3f}) + {:0.3f}, r^2={:0.3f}'.format(slope, 2*D*dt, intercept, r_value))
 
-#for t, x in enumerate(Xs[:,0,:]):
-#    print(t, ' '.join(map(str, x)))
+def mean_var():
+    # Mean and Variance check (e.g. for Ornstein-Uhlenbeck process)
+    stderr.write('beta = {}, 1/beta = {}, D = {}, 1/D = {}, beta*D = {}, 1/(beta*D)={}\n'.format(beta, 1/beta, D, 1/D, beta*D, 1/(beta*D)))
+    mean = np.mean(Xs[:,0,:], axis=1)
+    var = np.var(Xs[:,0,:], axis=1)
+    for t, m, v in zip(range(num_steps), mean, var):
+        print(t, m, v)
+
+def histogram():
+    pass
